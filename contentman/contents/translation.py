17c908d6f28769c63a2030e47a45ab8ef9dde4ff
user.name <gmail@hdknr.com>
2022-12-08 20:49:48 +0900

    find_node : find missiing 'lang_to' node in dict

--- contentman/contents/translation.py
+++ contentman/contents/translation.py
@@ -1,13 +1,11 @@
-def walk_translate(translated, node, path, lang_from="ja", lang_to="en", excludes=None):
-    for key, value in node.items():
-        current_path = f"{path}.{key}"
-        translate_path = f"{path}.{lang_to}"
-        if isinstance(value, dict):
-            walk_translate(translated, value, current_path, lang_from=lang_from, lang_to=lang_to, excludes=excludes)
-            continue
-        if key == lang_from:
-            if lang_to not in node and (not excludes or current_path not in excludes):
-                translated[translate_path] = value + f".{lang_to}"  # TODO
+def find_node(results, node, path, lang_from="ja", lang_to="en"):
+    if isinstance(node, dict):
+        for key, value in node.items():
+            current_path = f"{path}.{key}"
+            if key == lang_from and lang_to not in node:
+                results.append(current_path)
+            find_node(results, value, current_path, lang_from=lang_from, lang_to=lang_to)
+
 
 
 class Translator:
